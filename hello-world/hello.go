603dec553fb8507cf44d7753f7cb024cb0f4bfec
Raja Genupula <raja.genupula@yahoo.com>
2022-04-16 16:33:43 +0530

    added more cases to test and improved code refactoring

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 )
 
+const englishHelloPrefix = "Hello, "
+const defaultSuffix = "World"
+const frenchHelloPrefix = "Salut, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "spanish"
+const french = "french"
+
 func main() {
-	fmt.Println(Hello("Raja"))
+	fmt.Println(Hello("Raja", "english"))
 }
 
-func Hello(name string) string {
-	return "Hello, " + name + "!"
+func Hello(name, lang string) (prefix string) {
+	if name == "" {
+		name = defaultSuffix
+	}
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix + name
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,40 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Raja")
-	want := "Hello, Raja!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Raja", "english")
+		want := "Hello, Raja"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World', when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "english")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Salut, Raja' in french, when lang passed as french", func(t *testing.T) {
+		got := Hello("Raja", "french")
+		want := "Salut, Raja"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hola, Raja' in spansih, when lang passed as french", func(t *testing.T) {
+		got := Hello("Raja", "spanish")
+		want := "Hola, Raja"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
